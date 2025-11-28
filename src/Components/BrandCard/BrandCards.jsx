3eb8c5b934b4f82865e92b285f27321fbaec54cf
Zahra172 <ZaHra_magdi@protonmail.com>
2025-11-28 05:33:26 +0200

    Add some edits

--- src/Components/BrandCard/BrandCards.jsx
+++ src/Components/BrandCard/BrandCards.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
-export default function BrandCards({ image1, image2 ,w,h  }) {
+export default function BrandCards({ image1, image2  }) {
   return (
     <div className="flex flex-col items-center gap-6">
-      <img src={image1} alt="brand 1" className=" object-contain" />
-      <img src={image2} alt="brand 2" className=" w-40" style={{ width: w, height: h }} />
+      <img
+        src={image1}
+        alt="brand 1"
+        className="object-contain w-96 h-auto"  
+      />
+      <img
+        src={image2}
+        alt="brand 2"
+        className="w-20 h-auto"
+      />
     </div>
   );
 }
--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -10,29 +10,52 @@ export default function Brands() {
         <br /> of top DTC <span className="grace-title ">brands.</span>
       </h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-14 mt-6">
-        <BrandCards
-          image1="/src/assets/images/runGum_image.png " 
-          image2="/src/assets/images/rungum_text.png"
-          
-        />
-        <BrandCards
-          image1="/src/assets/images/bearBrand_image.png" 
-          image2="/src/assets/images/bearBrand_text.png"
-         
-        />
-        <BrandCards
-          image1="/src/assets/images/promix_image.png" 
-          image2="/src/assets/images/promix_text.png"
-          
-        />
-        <BrandCards
-          image1="/src/assets/images/scotch_image.png" 
-          image2="/src/assets/images/scotch_txt.png"
-          className="w-4 h-4"
-          w="60px"
-          h="32px"
-        />
+      <div className="grid grid-cols-2 gap-14 mt-6 overflow-hidden  max-h-[400px]">
+        
+        <div className="scroll-up space-y-6">
+          <BrandCards
+            image1="/src/assets/images/runGum_image.png"
+            image2="/src/assets/images/rungum_text.png"
+          />
+
+          <BrandCards
+            image1="/src/assets/images/bearBrand_image.png"
+            image2="/src/assets/images/bearBrand_text.png"
+          />
+          <BrandCards
+            image1="/src/assets/images/promix_image.png"
+            image2="/src/assets/images/promix_text.png"
+          />
+
+          <BrandCards
+            image1="/src/assets/images/scotch_image.png"
+            image2="/src/assets/images/scotch_txt.png"
+            
+          />
+        </div>
+
+       
+        <div className="scroll-down space-y-6">
+          <BrandCards
+            image1="/src/assets/images/promix_image.png"
+            image2="/src/assets/images/promix_text.png"
+          />
+
+          <BrandCards
+            image1="/src/assets/images/scotch_image.png"
+            image2="/src/assets/images/scotch_txt.png"
+            
+          />
+          <BrandCards
+            image1="/src/assets/images/runGum_image.png"
+            image2="/src/assets/images/rungum_text.png"
+          />
+
+          <BrandCards
+            image1="/src/assets/images/bearBrand_image.png"
+            image2="/src/assets/images/bearBrand_text.png"
+          />
+        </div>
       </div>
     </div>
   );
